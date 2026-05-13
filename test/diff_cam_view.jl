010060a021b7f18a4361e2f4b427fc826e96a189
Davis-Rippon <drip0002@student.monash.edu>
2026-05-13 20:35:20 +1000

    remove comment

--- test/diff_cam_view.jl
+++ test/diff_cam_view.jl
@@ -20,7 +20,7 @@ is_running = true
     im2 = read(camera)
 
     diff = mean(Float32.(channelview(im1)) .- Float32.(channelview(im2)), dims=1)[1,:,:]
-    obs[] = diff          # just updates the GPU texture, no replot
+    obs[] = diff
 end
 
 #%% Shut down
