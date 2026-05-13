58e9704cdad05dffdd41fcc2aca10a5db9b64f1e
Davis-Rippon <drip0002@student.monash.edu>
2026-05-13 12:12:00 +1000

    add more tests

--- test/test_compare.jl
+++ test/test_compare.jl
@@ -2,25 +2,24 @@
 include("src/block_match.jl")
 
 #%% Using
-using Images, StatsBase, ColorVectorSpace, Colors
+using Images, StatsBase, ColorVectorSpace, Colors, Plots
 using .BlockMatch
 
 #%%
 black = channelview(load("data/black.png"))[1:3,:,:]
 gray = channelview(load("data/gray.png"))[1:3,:,:]
 white = channelview(load("data/white.png"))[1:3,:,:]
-complex_original = channelview(load("data/hourglass.png"))[1:3,:,:]
-complex_shifted = channelview(load("data/hourglass-shifted.png"))[1:3,:,:]
+ref = channelview(load("data/hourglass.png"))[1:3,:,:]
+target = channelview(load("data/hourglass-shifted.png"))[1:3,:,:]
 
 c1 = channelview(load("data/circle1.png"))[1:3,:,:]
 c2 = channelview(load("data/circle2.png"))[1:3,:,:]
 
 
-complex_ba = construct_block_array(complex_shifted)
+complex_ba = construct_block_array(target)
 black_ba = construct_block_array(black)
-c_ba = construct_block_array(c1)
 
-mv = exhaustive_search(complex_ba, complex_original, 16, 16)
+mv = exhaustive_search(complex_ba, ref, 16, 16)
 bv = exhaustive_search(black_ba, black, 32, 16)
 
 #%% Plot V field
@@ -90,6 +89,34 @@ function plot_vector_field(
 end
 
 
-
-cv =  exhaustive_search(c_ba, c2, 100, 16)
+#%% Reconstructing C2
+target_ba = construct_block_array(c2)
+cv = exhaustive_search(target_ba, c1, 100, 16)
 plot_vector_field(cv)
+
+x = reconstruct_image(cv, c1, 16)
+img = colorview(RGB, x)
+plot(img .- colorview(RGB, pad_image(c2,16)), title="Residual Of C2 Reconstruction ")
+
+#%% Reconstructing c1
+target_ba = construct_block_array(c1)
+cv = exhaustive_search(target_ba, c2, 100, 16)
+plot_vector_field(cv)
+
+x = reconstruct_image(cv, c2, 16)
+img = colorview(RGB, x)
+plot(img .- colorview(RGB, pad_image(c1,16)), title="Residual of C1 Reconstruction")
+
+#%% Reconstructing Complex_shifted
+ref = channelview(load("data/hourglass.png"))[1:3,:,:]
+target = channelview(load("data/hourglass-shifted.png"))[1:3,:,:]
+
+target_ba = construct_block_array(target)
+cv = logarithmic_search(c_ba, ref, 150, 16)
+plot_vector_field(cv)
+
+x = reconstruct_image(cv, ref, 16)
+img = colorview(RGB, x)
+plot(img)
+plot(img .- colorview(RGB, pad_image(target,16)), title="Residual of C1 Reconstruction")
+
